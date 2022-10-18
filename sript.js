ce2d8907294f83a57a5e32234d740bf992cb1567
jgrant928 <jsgrant928@gmail.com>
2022-10-18 18:24:59 -0400

    completed JS

--- sript.js
+++ sript.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
         // get nearby values
+   
         var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
 
         // save in localStorage
-        localStorage.setItem('value', value);
+        localStorage.setItem(time, value);
 
         // Show notification that item was saved to localStorage by adding class 'show'
         $('.notification').addClass('show');
@@ -18,23 +20,51 @@ $(document).ready(function () {
 
     function hourUpdater() {
         // get current number of hours (preferably with moment.js)
+        var currentHour = moment().hours();
 
         // loop over time blocks
-        // loop over time blocks ---> https://api.jquery.com/each/
-        // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
+        var eachFunction = (function () {
+            var blockHour = parseInt($(this).attr('data-hour'));
 
-        // check if we've moved past this time
+
+        // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+        var timeBlock = $('.time-block').each(eachFunction)
     }
 
     hourUpdater();
 
     // set up interval to check if current time needs to be updated
     var interval = setInterval(hourUpdater, 15000);
+    
 
     // load any saved data from localStorage
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    ///need to repeat line 21 for all the other hours
 
+
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
     // display current day on page
+
+
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+
 });
